26af8db1322d6a90697bd8710101209cf33a2345
Violeta Calian <45428243+violetacln@users.noreply.github.com>
2025-09-25 11:26:00 +0000

    Update etl_checks.R

--- examples_rule_sourcing/etl_checks.R
+++ examples_rule_sourcing/etl_checks.R
@@ -9,13 +9,13 @@ library(arsenal)
 ## apply to the example datasets we have tested for rule-suggestions, etc.
 
 ## example data HAGS ------*--------
-d2 <- read.csv("C:/Users/violetac/OneDrive - Public Administration/Desktop/sdmx_validation_R_py/BCS_QBD_Q2_2023OllgognAnDataflow.csv", sep=";", header=TRUE)
+d2 <- read.csv("file_X.csv", sep=";", header=TRUE)
 
-d10 <- read.csv("C:/Users/violetac/OneDrive - Public Administration/Desktop/sdmx_validation_R_py/Bure_ini.csv", sep=";", header=TRUE)
+d10 <- read.csv("file_Y.csv", sep=";", header=TRUE)
 
-d_map <- read.csv("C:/Users/violetac/OneDrive - Public Administration/Desktop/sdmx_validation_R_py/isat_sdmx.csv", sep=";", header=TRUE)
+d_map <- read.csv("file_Z.csv", sep=";", header=TRUE)
 
-d1 <-     ### mapping betweeen d10 and d2, by using d_map
+#d1 <- map(d10)    ### mapping betweeen d10 and d2, by using d_map if needed
 
   
 summary(comparedf(d1,d2))
@@ -24,10 +24,9 @@ summary(comparedf(d1,d2))
 ## more detailed comparisons: with same function; with tidyverse, etc
 
 
-### anomaly detection ------------------
-
 ##....
 
 ###-------------------------------------
+
 
 
